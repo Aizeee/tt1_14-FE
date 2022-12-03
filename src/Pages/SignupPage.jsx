85c749d2355f42e5b068e0c4205b79bbbdcfffb4
nc1z <nc1zdev@gmail.com>
2022-12-03 11:15:13 +0800

    [SignupForm, SignupPage] Created initial Signup pages and components

---
+++ src/Pages/SignupPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignupForm from "../Components/Content/SignupForm";
+
+const Login = () => {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
+};
+
+export default Login;
