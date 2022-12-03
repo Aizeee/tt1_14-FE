63c14e07e519b0a20884e6b8cfed65c02510db22
Jia Hui <angjiahui90@gmail.com>
2022-12-03 14:27:38 +0800

    toggle between ammend and view

--- src/Components/Content/UserDetails.js
+++ src/Components/Content/UserDetails.js
@@ -4,26 +4,32 @@ import Col from 'react-bootstrap/Col'
 // import Cards from './Cards';
 import Form from 'react-bootstrap/Form'
 import React, { useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button'
 
 const mockuserdata = 
-  {
-  "UserID": 1,
-  "Username": "ExecutiveDBS",
-  "Password": "DBSBestBank2022",
-  "Firstname": "Tom",
-  "Lastname": "Lim",
-  "Email": "TomLim@easymail.com",
-  "Address": "Block 123 Serangoon Garden #10-129",
-  "OptIntoPhyStatements": 0
-  }
-  
-  const ContactForm = () => {
+    {
+    "UserID": 1,
+    "Username": "ExecutiveDBS",
+    "Password": "DBSBestBank2022",
+    "Firstname": "Tom",
+    "Lastname": "Lim",
+    "Email": "TomLim@easymail.com",
+    "Address": "Block 123 Serangoon Garden #10-129",
+    "OptIntoPhyStatements": 0
+    }
+
+
+const UserDetails = () => {
     const [userdata, setuserdata] = useState([]);
     // const [address, setAddress] = useState([]);
     useEffect(() => {
         setuserdata(mockuserdata)
         console.log(userdata);
       }, [setuserdata]);
+    //   useEffect(() => {
+    //     setAddress(userdata)
+    //   }, [address]);
+    
   return (
     <Container>
       <p> </p>
@@ -35,7 +41,8 @@ const mockuserdata =
           <Form.Label>Your Email Address</Form.Label>
         </Col>
         <Col>
-          <Form.Control placeholder={userdata.Email} />
+          {/* <Form.Control placeholder="" /> */}
+          <Form.Label>{userdata.Email}</Form.Label>
         </Col>
         </Row>
         <Row>
@@ -46,11 +53,16 @@ const mockuserdata =
           <Form.Label>Address</Form.Label>
         </Col>
         <Col>
-          <Form.Control placeholder={userdata.Address} />
+          {/* <Form.Control placeholder="" /> */}
+          <Form.Label>{userdata.Address}</Form.Label>
         </Col>
+        
+        </Row>
+        <Row>
+            
         </Row>
     </Container>
   );
 }
 
-export default ContactForm;
+export default UserDetails;
--- src/Pages/ExtraPage.js
+++ src/Pages/ExtraPage.js
@@ -5,16 +5,18 @@ import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import ContactForm from '../Components/Content/contactdetailform'
-// import AddressForm from '../Components/Content/addressForm'
-// import CountrySelect from 'react-bootstrap-country-select';
-
-// const [checked, setChecked] = useState(false);
+import UserDetails from '../Components/Content/UserDetails'
+// const [editForm] = useState(false);
 
 const Extra = () => {
-  // const handleClick = () => {state : }
+
+  const [editForm, setEditForm] = useState(false);
+  console.log(editForm)
+  const toggleForm = () => {
+    setEditForm(current => !current);
+  }
 
 
-  // }
   
   return (
     <div>
@@ -23,39 +25,44 @@ const Extra = () => {
       <h1>Update Personal and Contact Details</h1>
       <p>Please ensure that the details below are correct and click "Submit" to complete this transaction.</p>
      
-      <Form>
+      <Form onSubmit>
       <h3>Update of Personal Particulars</h3>
       <p> </p>
       <p> </p>
-      {/* <AddressForm/> */}
-      <ContactForm/>
-      {/* <h3>Consent and Withdrawal of Marketing Messages</h3>
-      <p> </p>
-      <Row>
-      <p>I would like the bank to</p>
-      </Row>
-      <Form.Group className="mb-3" id="formGridCheckbox" >
-        <Form.Check type="checkbox" label="My Mailing Address is the same as my residential address" defaultChecked = {true}/>
-      </Form.Group>
-      <ContactForm/> */}
+
+      {
+        editForm === true ? <ContactForm/> : <UserDetails/>
+      }
+      
     <Row>
     <p> </p>
       <p> </p>
-    <Col>
-    <Button variant="primary" type="submit">
-        cancel
-      </Button>
-    </Col>
-    <Col>
-    <Button variant="primary" type="submit">
-        Ammend
-      </Button>
-    </Col>
-    <Col>
-    <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Col>
+    
+    {
+        editForm === false ? "" : <Col>
+    
+        <Button variant="primary" type="submit" onClick={toggleForm}>
+            Cancel
+          </Button>
+        </Col>
+      }
+    {
+        editForm === true ? "" : <Col>
+    
+        <Button variant="primary" type="submit" onClick={toggleForm}>
+            Ammend
+          </Button>
+        </Col>
+      }
+    {
+        editForm === false ? "" : <Col>
+    
+        <Button variant="primary" type="submit" onClick={toggleForm}>
+            Submit
+          </Button>
+        </Col>
+      }
+   
     </Row>
 
     </Form>
@@ -64,4 +71,3 @@ const Extra = () => {
 }
 
 export default Extra
-
