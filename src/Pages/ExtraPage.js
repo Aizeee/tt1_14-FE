7ce82e998f11a0a7014853e22d409db49dca851f
Jia Hui <angjiahui90@gmail.com>
2022-12-03 12:50:41 +0800

    complete interface of update mailing address

--- src/Pages/ExtraPage.js
+++ src/Pages/ExtraPage.js
@@ -3,15 +3,15 @@ import NavBar1 from '../Components/NavBars/NavBar1'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
-import AddressForm from '../Components/Content/addressForm'
+import Col from 'react-bootstrap/Col'
+// import AddressForm from '../Components/Content/addressForm'
 // import CountrySelect from 'react-bootstrap-country-select';
 
 // const [checked, setChecked] = useState(false);
 
 const Extra = () => {
+  // const handleClick = () => {state : }
 
-  // const handleClick = (accountId) => {
-  //   } }
 
   // }
   
@@ -19,30 +19,43 @@ const Extra = () => {
     <div>
       <NavBar1 />
       
-      <h1>Update Mailing Address</h1>
-      <p>some text abc</p>
-      <p>some text cde</p>
+      <h1>Update Personal and Contact Details</h1>
+      <p>Please ensure that the details below are correct and click "Submit" to complete this transaction.</p>
      
       <Form>
       <h3>My Residential Address</h3>
       <p> </p>
       <p> </p>
-      <AddressForm/>
-       
-        {/* country --> drop down; postal code */}
-        <h3>My Mailing Address</h3>
+      {/* <AddressForm/> */}
+              <h3>My Mailing Address</h3>
       <p> </p>
       <Row>
 
       </Row>
       <Form.Group className="mb-3" id="formGridCheckbox" >
-        <Form.Check type="checkbox" label="My Mailing Address is the same as my residential address" defaultChecked = {false}/>
+        <Form.Check type="checkbox" label="My Mailing Address is the same as my residential address" defaultChecked = {true}/>
       </Form.Group>
-      <AddressForm/>
-    
-      <Button variant="primary" type="submit">
-        Save
+      {/* <AddressForm/> */}
+    <Row>
+    <p> </p>
+      <p> </p>
+    <Col>
+    <Button variant="primary" type="submit">
+        cancel
       </Button>
+    </Col>
+    <Col>
+    <Button variant="primary" type="submit">
+        Ammend
+      </Button>
+    </Col>
+    <Col>
+    <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Col>
+    </Row>
+
     </Form>
     </div>
   )
--- src/Pages/UpdateMailingAddress.js
+++ src/Pages/UpdateMailingAddress.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import NavBar1 from '../Components/NavBars/NavBar1'
 import Button from 'react-bootstrap/Button'
-import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import AddressForm from '../Components/Content/addressForm'
+// import CountrySelect from 'react-bootstrap-country-select';
 
 // const [checked, setChecked] = useState(false);
 
 const Extra = () => {
+  // const handleClick = () => {state : }
 
-  // const handleClick = (accountId) => {
-  //   } }
 
   // }
   
@@ -26,114 +27,42 @@ const Extra = () => {
       <h3>My Residential Address</h3>
       <p> </p>
       <p> </p>
-        <Row>
-        <Col>
-          <Form.Label>Postal Code</Form.Label>
-        </Col>
-        <Col>
-          <Form.Control placeholder="" />
-        </Col>
-        </Row>
-        <Row>
-          <p></p>
-        </Row>
-        <Row>
-        <Col>
-          <Form.Label>Block No.</Form.Label>
-        </Col>
-        <Col>
-          <Form.Control placeholder="" />
-        </Col>
-        </Row>
-        <Row>
-          <p></p>
-        </Row>
-        <Row>
-        <Col>
-          <Form.Label>Unit No.</Form.Label>
-        </Col>
-        <Col>
-          <Form.Control placeholder="" />
-        </Col>
-        <Col>
-          <Form.Label>-</Form.Label>
-        </Col>
-        <Col>
-          <Form.Control placeholder="" />
-        </Col>
-        </Row>
-        <Row>
-          <p></p>
-        </Row>
-        <Row>
-        <Col>
-          <Form.Label>Street Name</Form.Label>
-        </Col>
-        <Col>
-          <Form.Control placeholder="" />
-        </Col>
-        </Row>
+      <AddressForm/>
+       
         {/* country --> drop down; postal code */}
         <h3>My Mailing Address</h3>
       <p> </p>
+      <Row>
+
+      </Row>
       <Form.Group className="mb-3" id="formGridCheckbox" >
-        <Form.Check type="checkbox" label="My Mailing Address is the same as my residential address" defaultChecked = {false}/>
+        <Form.Check type="checkbox" label="My Mailing Address is the same as my residential address" defaultChecked = {true}/>
       </Form.Group>
+      <AddressForm/>
+    <Row>
+    <p> </p>
       <p> </p>
-        <Row>
-        <Col>
-          <Form.Label>Postal Code</Form.Label>
-        </Col>
-        <Col>
-          <Form.Control placeholder="" />
-        </Col>
-        </Row>
-        <Row>
-          <p></p>
-        </Row>
-        <Row>
-        <Col>
-          <Form.Label>Block No.</Form.Label>
-        </Col>
-        <Col>
-          <Form.Control placeholder="" />
-        </Col>
-        </Row>
-        <Row>
-          <p></p>
-        </Row>
-        <Row>
-        <Col>
-          <Form.Label>Unit No.</Form.Label>
-        </Col>
-        <Col>
-          <Form.Control placeholder="" />
-        </Col>
-        <Col>
-          <Form.Label>-</Form.Label>
-        </Col>
-        <Col>
-          <Form.Control placeholder="" />
-        </Col>
-        </Row>
-        <Row>
-          <p></p>
-        </Row>
-        <Row>
-        <Col>
-          <Form.Label>Street Name</Form.Label>
-        </Col>
-        <Col>
-          <Form.Control placeholder="" />
-        </Col>
-        </Row>
-    
-      <Button variant="primary" type="submit">
-        Save
+    <Col>
+    <Button variant="primary" type="submit">
+        cancel
       </Button>
+    </Col>
+    <Col>
+    <Button variant="primary" type="submit">
+        Ammend
+      </Button>
+    </Col>
+    <Col>
+    <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Col>
+    </Row>
+
     </Form>
     </div>
   )
 }
 
 export default Extra
+
